48df7d572e9b0bb56413bc300525adf75cbffb68
azmir849 <azmir15-849@diu.edu.bd>
2022-03-06 01:26:16 +0600

    global style added

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from "react";
 import { BrowserRouter as Router } from 'react-router-dom';
 import Navbar from './components/Navbar'
+import { GlobalStyle } from "./globalStyles";
 
 function App() {
   return (
     <Router>
-     <Navbar/>
+       <GlobalStyle />
+     <Navbar />
     </Router>
   );
 }
