bc58f76e4e94cfbc73c6878c4ca7335adc9cda9d
azmir849 <azmir15-849@diu.edu.bd>
2022-03-06 12:56:25 +0600

    feature component

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import { BrowserRouter as Router } from 'react-router-dom';
 import Hero from './components/Hero';
 import Products from './components/Products';
 import { productData, productDataTwo } from './components/Products/data';
+import Feature from './components/Feature';
 import { GlobalStyle } from "./globalStyles";
 
 function App() {
@@ -11,6 +12,7 @@ function App() {
        <GlobalStyle />
      <Hero />
      <Products heading='Choose your favorite one' data={productData} />
+     <Feature />
      <Products heading='Sweet Treats for You' data={productDataTwo} />
     </Router>
   );
