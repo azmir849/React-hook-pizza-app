588fcdbb5f7268775e56c2fdffb083f05d96f290
azmir849 <azmir15-849@diu.edu.bd>
2022-03-06 13:20:02 +0600

    footer component

---
+++ src/components/Footer/index.js
@@ -0,0 +1,57 @@
+import React from 'react';
+import {
+  FaFacebook,
+  FaInstagram,
+  FaYoutube,
+  FaTwitter,
+  FaLinkedin
+} from 'react-icons/fa';
+import {
+  FooterContainer,
+  FooterWrap,
+  SocialMedia,
+  SocialMediaWrap,
+  Logo,
+  CopyRight,
+  SocialIcons,
+  SocialIconLink
+} from './FooterElements';
+
+const Footer = () => {
+  return (
+    <FooterContainer>
+      <FooterWrap>
+        <SocialMedia>
+          <SocialMediaWrap>
+            <Logo to='/'>Pizza</Logo>
+            <CopyRight to='/'>All right reserved &copy;Azmir Hossen (AH Naeem)</CopyRight>
+            <SocialIcons>
+              <SocialIconLink href='/' target='_blank' aria-label='Facebook'>
+                <FaFacebook />
+              </SocialIconLink>
+              <SocialIconLink href='/' target='_blank' aria-label='Instagram'>
+                <FaInstagram />
+              </SocialIconLink>
+              <SocialIconLink href='/' target='_blank' aria-label='Youtube'>
+                <FaYoutube />
+              </SocialIconLink>
+              <SocialIconLink
+                href=''
+                target='_blank'
+                aria-label='Twitter'
+                rel='noopener noreferrer'
+              >
+                <FaTwitter />
+              </SocialIconLink>
+              <SocialIconLink href='/' target='_blank' aria-label='Linkedin'>
+                <FaLinkedin />
+              </SocialIconLink>
+            </SocialIcons>
+          </SocialMediaWrap>
+        </SocialMedia>
+      </FooterWrap>
+    </FooterContainer>
+  );
+};
+
+export default Footer;
